bf9a858df493d0942f2e9688081ebff45a4aa108
Julia Fisher <juliafisher@Julias-MacBook-Pro.local>
2022-02-21 09:37:53 -0500

    Added guinea pig

---
+++ src/com/company/GuineaPig.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class GuineaPig {
+
+    //set properties
+    private String guineaPigName;
+    private int guineaPigAge;
+    private int guineaPigWeight;
+
+    //setters
+    public GuineaPig(String guineaPigName, int guineaPigAge, int guineaPigWeight) {
+        this.guineaPigName = guineaPigName;
+        this.guineaPigAge = guineaPigAge;
+        this.guineaPigWeight = guineaPigWeight;
+}
+
+//getters
+public String getGuineaPigName() {
+    return guineaPigName;
+        }
+        public int getguineaPigAge() {
+            return guineaPigAge;
+        }
+        public int getGuineaPigWeight() {
+            return guineaPigWeight;
+        }
+        }
+
